b3f022454c83ea57b043d573d226c0004e3717c0
Gal Orazem <go7487@student.uni-lj.si>
2023-08-18 15:46:52 +0200

    Zajem spletnih strani

--- zajem.py
+++ zajem.py
@@ -1,4 +1,18 @@
 import re
 import orodja
 
+for i in range(1, 16):
+    url = (
+        'https://firstcycling.com/ranking.php?h=1&rank=1&y=2023&page={}&wom=1'
+    ).format(i)
+    orodja.shrani_spletno_stran(
+        url, 'strani/kolesar_{}.html'.format(i)
+)
+
+
+
+
+
+
+
 
