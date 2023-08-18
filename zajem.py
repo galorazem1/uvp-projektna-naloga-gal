630dc425719184b3c717723e90ee2babc00620f6
Gal Orazem <go7487@student.uni-lj.si>
2023-08-18 13:41:29 +0200

    Uvozil orodja.py (sestavil prof. Pretnar).

--- zajem.py
+++ zajem.py
@@ -1,9 +1,4 @@
-import csv
-import os
-import requests
 import re
+import orodja
 
-glasba_frontpage_url = 'https://rateyourmusic.com/charts/top/album/all-time/'
-glasba_directory = 'glasba'
-frontpage_filename = 'glasba.html'
-csv_filename = 'glasba.csv'
+
