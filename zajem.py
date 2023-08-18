8870f44d914a4e970e0ec45d76a31e3a6bab2bdf
Gal Orazem <go7487@student.uni-lj.si>
2023-08-18 12:59:33 +0200

    Poizkus delovanja Commit-a.

---
+++ zajem.py
@@ -0,0 +1,9 @@
+import csv
+import os
+import requests
+import re
+
+glasba_frontpage_url = 'https://rateyourmusic.com/charts/top/album/all-time/'
+glasba_directory = 'glasba'
+frontpage_filename = 'glasba.html'
+csv_filename = 'glasba.csv'
